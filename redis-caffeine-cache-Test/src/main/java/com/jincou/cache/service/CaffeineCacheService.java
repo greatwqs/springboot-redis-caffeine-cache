981ec64a2c6f23cb3ec4e9e20d19e62bc5dd01c6
greatwqs <greatwqs@163.com>
2023-08-22 17:05:00 +0800

    Update CaffeineCacheService.java
    TestCase Cache:
    PUT: http://localhost:8084/putUser?userId=1001
    GET: http://localhost:8084/queryUser?userId=1001
    
    获取单项缓存时:
    @Cacheable(value = "userCache", key = "#userId")
    public UserDTO queryUser(String userId)
    
    获取list缓存时:
    @Cacheable(value = "userCache-list", key = "#userId", sync = true)
    public List<UserDTO> queryUserSyncList(String userId)

--- redis-caffeine-cache-Test/src/main/java/com/jincou/cache/service/CaffeineCacheService.java
+++ redis-caffeine-cache-Test/src/main/java/com/jincou/cache/service/CaffeineCacheService.java
@@ -42,7 +42,7 @@ public class CaffeineCacheService {
      * 获取或加载缓存项
      * <p>
      */
-    @Cacheable(key = "'cache_user_id_' + #userId", value = "userCache")
+    @Cacheable(value = "userCache", key = "#userId")
     public UserDTO queryUser(String userId) {
         UserDTO userDTO = userMap.get(userId);
         try {
@@ -64,7 +64,7 @@ public class CaffeineCacheService {
      * sync=false，是通过get(Object key)来获取缓存项，由于没有valueLoader（加载缓存项的具体逻辑），所以CaffeineCache在定时刷新过期缓存时，缓存项过期则会被淘汰。
      * <p>
      */
-    @Cacheable(value = "userCache", key = "#userId", sync = true)
+    @Cacheable(value = "userCache-list", key = "#userId", sync = true)
     public List<UserDTO> queryUserSyncList(String userId) {
         UserDTO userDTO = userMap.get(userId);
         List<UserDTO> list = new ArrayList();
